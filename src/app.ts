f60e9a86521c2aa8d2d8c976cb254ceefe7fb40c
Davi Silva <davi.silva@code7.com>
2021-04-22 11:47:26 -0300

    feat(app): comments out for test
    comments out code for tests

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import OrchyBase from 'orchy-base-code7';
 import SnsSqsSlq from 'sns-sqs-slq-code7';
 
-import { associatesQueueWithLambda } from './utils/aws/index';
+// import { associatesQueueWithLambda } from './utils/aws/index';
 
 import 'dotenv/config';
 
@@ -23,52 +23,57 @@ async function getQueueContacts() {
   console.log(queueContacts);
 
   if (queueContacts.length > 0) {
-    const topic = await snsSqs.createOrGetTopic('sns-event');
+    // const topic = await snsSqs.createOrGetTopic('sns-event');
 
     queueContacts.forEach(async (queueContact) => {
-      const queueInfo = await snsSqs.createOrGetQueue(
-        `sqs-event-${queueContact.api_key}`,
-      );
+      console.log(queueContact);
+      // const queueInfo = await snsSqs.createOrGetQueue(
+      //   `sqs-event-${queueContact.api_key}`,
+      // );
 
-      const queueName = queueInfo.QueueUrl.split('/')[4];
+      // const queueName = queueInfo.QueueUrl.split('/')[4];
 
-      const sub = await snsSqs.subscribeToTopic(
-        topic.TopicArn,
-        `arn:aws:sqs:${process.env.AWS_REGION}:${process.env.AWS_ID_ACCOUNT}:${queueName}`,
-        queueInfo.QueueUrl,
-      );
+      // const sub = await snsSqs.subscribeToTopic(
+      //   topic.TopicArn,
+      //   `arn:aws:sqs:${process.env.AWS_REGION}:${process.env.AWS_ID_ACCOUNT}:${queueName}`,
+      //   queueInfo.QueueUrl,
+      // );
 
-      // console.log('sub:', sub);
+      // // console.log('sub:', sub);
 
-      snsSqs.setFilterPolicyAttributeInSubscription(sub.SubscriptionArn, {
-        api_key: [queueContact.api_key],
-      });
+      // snsSqs.setFilterPolicyAttributeInSubscription(sub.SubscriptionArn, {
+      //   api_key: [queueContact.api_key],
+      // });
 
-      console.log('queueName:', queueName);
+      // console.log('queueName:', queueName);
 
-      associatesQueueWithLambda(queueName);
+      // associatesQueueWithLambda(queueName);
 
-      await snsSqs.publishToTopic(
-        topic.TopicArn.split(':')[5],
-        JSON.stringify({
-          id_flow: queueContact.id_flow,
-          api_key: queueContact.api_key,
-          id_contact_data: queueContact.id_contact_data,
-          id_item: queueContact.id_item,
-          event_type: queueContact.event_type,
-          data_type: queueContact.data_type,
-          contact_data: queueContact.contact_data,
-          message: queueContact.message,
-        }),
-        Date.now().toString(),
-        Date.now().toString(),
-        topic.TopicArn,
-        {
-          api_key: {
-            DataType: 'String',
-            StringValue: queueContact.api_key,
-          },
-        },
+      // await snsSqs.publishToTopic(
+      //   topic.TopicArn.split(':')[5],
+      //   JSON.stringify({
+      //     id_flow: queueContact.id_flow,
+      //     api_key: queueContact.api_key,
+      //     id_contact_data: queueContact.id_contact_data,
+      //     id_item: queueContact.id_item,
+      //     event_type: queueContact.event_type,
+      //     data_type: queueContact.data_type,
+      //     contact_data: queueContact.contact_data,
+      //     message: queueContact.message,
+      //   }),
+      //   Date.now().toString(),
+      //   Date.now().toString(),
+      //   topic.TopicArn,
+      //   {
+      //     api_key: {
+      //       DataType: 'String',
+      //       StringValue: queueContact.api_key,
+      //     },
+      //   },
+      // );
+
+      await snsSqs.clearEventSourcesFromLambda(
+        'lbd-contact-event-dev-contactEvent',
       );
 
       // await snsSqs.dele;
